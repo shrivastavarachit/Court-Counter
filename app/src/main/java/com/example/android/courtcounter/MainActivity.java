685b06d7696589d03310cc3e0fe41313930b1810
RACHIT SHRIVASTAVA <1998.rachit@gmail.com>
2019-03-19 17:10:57 +0530

    Second

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -2,11 +2,47 @@ package com.example.android.courtcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main_menu , menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+         super.onOptionsItemSelected(item);
+
+         switch (item.getItemId())
+         {
+             case R.id.Reset:
+                 scoreTeamA = 0;
+                 scoreTeamB = 0;
+                 displayForTeamA(scoreTeamA);
+                 displayForTeamB(scoreTeamB);
+                 return true;
+
+             case R.id.Settings:
+                 Toast.makeText(this , "Settings working", Toast.LENGTH_LONG).show();
+                 return true;
+
+                 default:
+                     return false;
+         }
+    }
 
     int scoreTeamA = 0;
 
